7d200f6db46a0656d03a5fe554de6482419ee939
Leandro Doctors <ldoctors@untref.edu.ar>
2020-11-16 19:45:38 -0300

    Corregir tests

--- src/test/PruebaReglasCuatroEnLinea.java
+++ src/test/PruebaReglasCuatroEnLinea.java
@@ -206,19 +206,18 @@ public class PruebaReglasCuatroEnLinea {
 	public void empatanEnTableroDe5Por6() {
 	
 		juego = new CuatroEnLinea(5, 6, "Jime", "Nico");
-		fueronSoltadasFichasEnColumnas(1,2,3,4,4,3,2,1,4,3,2,1,1,2,3,4);
-		
-		juego.soltarFichaEnColumna(4);
+		fueronSoltadasFichasEnColumnas(1, 2, 3, 4, 5, 6, 6, 5, 4, 3, 2, 1, 6,
+				5, 4, 3, 2, 1, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5);
+		juego.soltarFichaEnColumna(6);
 
 		asertarQueEmpataron();
 	}
 	
 	@Test
-	public void empatanEnTableroDe4Por() {
-	
+	public void empatanEnTableroDe4Por4() {
 		juego = new CuatroEnLinea(4, 4, "Nati", "Fabi");
-		fueronSoltadasFichasEnColumnas();
-		
+		fueronSoltadasFichasEnColumnas(1, 2, 3, 4, 4, 3, 2, 1, 4, 3, 2, 1, 4,
+				3, 2);
 		juego.soltarFichaEnColumna(1);
 
 		asertarQueEmpataron();

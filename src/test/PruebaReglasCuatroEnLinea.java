7a00faff81bd641cff41bb83b35bfcd9f8b6047f
Leandro Doctors <ldoctors@untref.edu.ar>
2020-11-16 19:48:08 -0300

    Organizar importaciones

--- src/test/PruebaReglasCuatroEnLinea.java
+++ src/test/PruebaReglasCuatroEnLinea.java
@@ -1,13 +1,12 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import juego.CuatroEnLinea;
 
 import org.junit.Test;
-
-import juego.CuatroEnLinea;
 
 public class PruebaReglasCuatroEnLinea {
 
